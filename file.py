c5cdcb44502b7b95417e7d1e27d22800f3a01c65
tereshenko35ftgr <144949807+tereshenko35ftgr@users.noreply.github.com>
2024-03-14 13:58:52 +0300

    Add files via upload

---
+++ file.py
@@ -0,0 +1,19 @@
+import random as rn
+a = rn.randint(1,100)
+trye = 1
+print("Привет, ты попал в угадай число от 1 до 100. Приступай!")
+while True:
+    u = int(input("Попытка " + str(trye) + ": "))
+    trye += 1
+    if a == u:
+        print("Ты выиграл!")
+        break
+    elif a <= u:
+     print("Загаданное число меньше.")
+    elif a >= u:
+        print("Загаданное число больше.")
+    else:
+        print("Не понял.")
+    if trye == 11:
+        print("Ты истратил все 10 попыток. Загаданное число: " + str(a))
+        break
